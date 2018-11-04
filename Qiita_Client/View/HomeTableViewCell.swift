5f311aec446b487948b79d5dc5d9d0fbb66c8ef8
tomoki69386 <tomoki_69386@yahoo.co.jp>
2018-11-04 18:25:40 +0900

    Update

---
+++ Qiita_Client/View/HomeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeTableViewCell.swift
+//  Qiita_Client
+//
+//  Created by 築山朋紀 on 2018/11/04.
+//  Copyright © 2018 tomoki. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Qiita_Client/View/User/UserViewController.swift
@@ -0,0 +1,36 @@
+//
+//  UserViewController.swift
+//  Qiita_Client
+//
+//  Created by 築山朋紀 on 2018/11/04.
+//  Copyright © 2018 tomoki. All rights reserved.
+//
+
+import UIKit
+import SkeletonView
+
+class UserViewController: UIViewController {
+    
+    @IBOutlet private weak var label: UILabel!
+    @IBOutlet private weak var ImageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        label.numberOfLines = 0
+        label.showAnimatedGradientSkeleton()
+        ImageView.showAnimatedGradientSkeleton()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
