6e64519b3d355179a099d38cf13c9fecf024ed94
tomoki69386 <tomoki_69386@yahoo.co.jp>
2018-11-05 17:31:37 +0900

    Create ErrorMessage

---
+++ Qiita_Client/ErrorMessage.swift
@@ -0,0 +1,33 @@
+//
+//  ErrorMessage.swift
+//  Qiita_Client
+//
+//  Created by 築山朋紀 on 2018/11/05.
+//  Copyright © 2018 tomoki. All rights reserved.
+//
+
+import Foundation
+
+struct ErrorMessage {
+    
+//    static func message(code: Int) {
+//        //200、201、204、400、401、403、404、500
+//        switch code {
+//        case 200:
+//            <#code#>
+//        case 201:
+//
+//        case 204:
+//
+//        case 400:
+//
+//        case 403:
+//
+//        case 404:
+//
+//        case 500:
+//            
+//        default: break
+//        }
+//    }
+}
