4e6253d4f0cff076f9cb4c16185ed41981fce47b
tomoki69386 <tomoki_69386@yahoo.co.jp>
2018-11-11 03:05:23 +0900

    Update

---
+++ Qiita_Client/View/User/UserArticleViewController.swift
@@ -0,0 +1,19 @@
+//
+//  UserArticleViewController.swift
+//  Qiita_Client
+//
+//  Created by 築山朋紀 on 2018/11/11.
+//  Copyright © 2018 tomoki. All rights reserved.
+//
+
+import UIKit
+import XLPagerTabStrip
+
+class UserArticleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
