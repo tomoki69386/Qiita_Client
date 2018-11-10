11265337a57fedcf5e1199c877c70991f47b5f60
tomoki69386 <tomoki_69386@yahoo.co.jp>
2018-11-10 10:33:37 +0900

    Update barColor

--- Qiita_Client/View/User/UserViewController.swift
+++ Qiita_Client/View/User/UserViewController.swift
@@ -8,15 +8,37 @@
 
 import UIKit
 import AMScrollingNavbar
-import Alamofire
+import SnapKit
 
 class UserViewController: UIViewController {
+    
+    let scrollView = UIScrollView()
+    let imageView = UIImageView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.title = "プロフィール"
+        imageView.backgroundColor = AppColor.glay
         showUser()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        self.view.addSubview(scrollView)
+        scrollView.addSubview(imageView)
+        
+        scrollView.snp.makeConstraints { (make) in
+            make.size.equalToSuperview()
+        }
+        imageView.snp.makeConstraints { (make) in
+            make.top.equalTo(10)
+            make.left.equalTo(10)
+            make.width.height.equalTo(90)
+            imageView.layer.cornerRadius = 45
+            imageView.clipsToBounds = true
+        }
     }
     
     private func showUser() {
