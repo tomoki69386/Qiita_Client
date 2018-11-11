0fe66fa0a20d1887c62a9605b2fa84e18863406e
tomoki69386 <tomoki_69386@yahoo.co.jp>
2018-11-11 22:38:45 +0900

    Add ProfileView

---
+++ Qiita_Client/View/User/ProfileView.swift
@@ -0,0 +1,26 @@
+//
+//  ProfileView.swift
+//  Qiita_Client
+//
+//  Created by 築山朋紀 on 2018/11/11.
+//  Copyright © 2018 tomoki. All rights reserved.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+    
+    
+    
+    required override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+}
