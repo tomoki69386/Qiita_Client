eeb4fb575e0a750aaaec0696ab5d1b291bcb7e7a
tomoki69386 <tomoki_69386@yahoo.co.jp>
2018-11-11 13:38:35 +0900

    Update

--- Qiita_Client/View/HomeViewController.swift
+++ Qiita_Client/View/HomeViewController.swift
@@ -7,73 +7,19 @@
 //
 
 import UIKit
-import AMScrollingNavbar
-import SkeletonView
+import XLPagerTabStrip
 
-class HomeViewController: MainViewController {
+class HomeViewController: ButtonBarPagerTabStripViewController {
     
-    private var items = [Article]()
-    
-    private let tableView = UITableView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        tableView.register(HomeTableViewCell.self, forCellReuseIdentifier: "Home")
-        tableView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
-        view.addSubview(tableView)
-        tableView.dataSource = self
-        tableView.delegate = self
-        tableView.showAnimatedGradientSkeleton()
-        
-        navigationItem.title = "記事一覧"
-        navigationController?.navigationBar.barTintColor = AppColor.main
-        navigationController?.navigationBar.tintColor = .white
-        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
-        
-        showRequest()
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        if let navigationController = navigationController as? ScrollingNavigationController {
-            navigationController.followScrollView(tableView, delay: 50.0)
-        }
-    }
-    
-    private func showRequest() {
-        items.removeAll()
-        APIClient.fetchArticle { (articles) in
-            self.items = articles
-            DispatchQueue.main.sync {
-                self.tableView.hideSkeleton()
-                self.tableView.reloadData()
-            }
-        }
-    }
-}
-
-extension HomeViewController: SkeletonTableViewDataSource {
-    
-    func collectionSkeletonView(_ skeletonView: UITableView, cellIdentifierForRowAt indexPath: IndexPath) -> ReusableCellIdentifier {
-        return "Home"
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Home", for: indexPath) as! HomeTableViewCell
-        cell.setup(item: items[indexPath.row])
-        return cell
-    }
-}
-
-extension HomeViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        let VC = ArticleViewController(article: items[indexPath.row])
-        self.navigationController?.pushViewController(VC, animated: true)
+    override func viewControllers(for pagerTabStripController: PagerTabStripViewController) -> [UIViewController] {
+        //管理されるViewControllerを返す処理
+        let newArticleVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "First")
+        let childViewControllers:[UIViewController] = [newArticleVC]
+        return childViewControllers
     }
 }
