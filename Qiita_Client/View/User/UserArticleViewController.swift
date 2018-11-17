deee8b5e7b938dfbe86bf1d2cd4d8f8fc0d3636b
tomoki69386 <tomoki_69386@yahoo.co.jp>
2018-11-18 06:02:11 +0900

    Update

--- Qiita_Client/View/User/UserArticleViewController.swift
+++ Qiita_Client/View/User/UserArticleViewController.swift
@@ -15,6 +15,8 @@ class UserArticleViewController: MainViewController, IndicatorInfoProvider {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = AppColor.main
     }
     
     func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
--- Qiita_Client/View/User/UserViewController.swift
+++ Qiita_Client/View/User/UserViewController.swift
@@ -15,9 +15,16 @@ class UserViewController: ButtonBarPagerTabStripViewController {
     let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
+        setBarLayout()
         super.viewDidLoad()
         
         navigationItem.title = "プロフィール"
+    }
+    
+    override func viewControllers(for pagerTabStripController: PagerTabStripViewController) -> [UIViewController] {
+        return [
+            Storyboard.userArticle.instantiateViewController()
+        ]
     }
     
     private func showUser() {
@@ -30,6 +37,21 @@ class UserViewController: ButtonBarPagerTabStripViewController {
             print(response)
         }) { (error) in
             print(error)
+        }
+    }
+    
+    private func setBarLayout() {
+        settings.style.buttonBarBackgroundColor = .white
+        settings.style.buttonBarItemBackgroundColor = .white
+        settings.style.buttonBarItemTitleColor = AppColor.main
+        settings.style.selectedBarBackgroundColor = AppColor.main
+        settings.style.selectedBarHeight = 2.5
+        changeCurrentIndexProgressive = { (oldCell: ButtonBarViewCell?, newCell: ButtonBarViewCell?, progressPercentage: CGFloat, changeCurrentIndex: Bool, animated: Bool) in
+            guard changeCurrentIndex else { return }
+            oldCell?.label.textColor = .gray
+            oldCell?.label.font = UIFont.systemFont(ofSize: CGFloat(15))
+            newCell?.label.textColor = AppColor.main
+            newCell?.label.font = UIFont.boldSystemFont(ofSize: 15)
         }
     }
 }
