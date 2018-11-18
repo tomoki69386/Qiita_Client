15b483e337db99f6564bbe1375ad3714c9d2722f
tomoki69386 <tomoki_69386@yahoo.co.jp>
2018-11-18 21:06:55 +0900

    Add HomeTrendTableViewCell

---
+++ Qiita_Client/View/Cell/HomeTrendTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeTrendTableViewCell.swift
+//  Qiita_Client
+//
+//  Created by 築山朋紀 on 2018/11/18.
+//  Copyright © 2018 tomoki. All rights reserved.
+//
+
+import UIKit
+
+class HomeTrendTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
