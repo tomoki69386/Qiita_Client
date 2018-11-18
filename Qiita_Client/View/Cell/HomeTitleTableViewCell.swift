b8db5548a4ae910641dbe1e755f7213c7be49e50
tomoki69386 <tomoki_69386@yahoo.co.jp>
2018-11-18 20:42:01 +0900

    Create HomeTitleTableViewCell

--- Qiita_Client/View/Cell/HomeTitleTableViewCell.swift
+++ Qiita_Client/View/Cell/HomeTitleTableViewCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SnapKit
 
 class HomeTitleTableViewCell: UITableViewCell {
     
@@ -14,9 +15,30 @@ class HomeTitleTableViewCell: UITableViewCell {
         let label = UILabel()
         label.text = "Qiita View"
         label.font = .systemFont(ofSize: 30)
+        label.textColor = AppColor.black
         return label
     }()
-
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        self.addSubview(titleLabel)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        titleLabel.snp.makeConstraints { make in
+            make.left.right.equalTo(self).offset(10)
+            make.height.equalTo(50)
+            make.top.bottom.equalTo(self.contentView)
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
