ab2e06342dfd7d4a7830b7e9d03bd2057f4fff12
tomoki69386 <tomoki_69386@yahoo.co.jp>
2018-11-18 19:21:20 +0900

    Add tableview

--- Qiita_Client/View/User/UserArticleViewController.swift
+++ Qiita_Client/View/User/UserArticleViewController.swift
@@ -9,16 +9,30 @@
 import UIKit
 import XLPagerTabStrip
 
-class UserArticleViewController: MainViewController, IndicatorInfoProvider {
+class UserArticleViewController: MainViewController {
     
-    private var itemInfo: IndicatorInfo = "投稿記事"
+    @IBOutlet private weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
+}
+
+extension UserArticleViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UserArticleCell", for: indexPath)
+        cell.textLabel?.text = String(indexPath.row)
+        return cell
+    }
+}
+
+extension UserArticleViewController: IndicatorInfoProvider {
     func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
-        return itemInfo
+        return "投稿"
     }
 }
