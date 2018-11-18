f504b048152dfa6e5b9e8b8e7e2625d0581dfed8
tomoki69386 <tomoki_69386@yahoo.co.jp>
2018-11-18 20:17:18 +0900

    Add HomeTitleTableViewCell'

---
+++ Qiita_Client/View/Cell/HomeTitleTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeTitleTableViewCell.swift
+//  Qiita_Client
+//
+//  Created by 築山朋紀 on 2018/11/18.
+//  Copyright © 2018 tomoki. All rights reserved.
+//
+
+import UIKit
+
+class HomeTitleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
