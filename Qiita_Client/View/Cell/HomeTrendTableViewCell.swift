19d245b33546527ea1fb08bd296dcc7b3cbe0a14
tomoki69386 <tomoki_69386@yahoo.co.jp>
2018-11-18 22:08:49 +0900

    Update

--- Qiita_Client/View/Cell/HomeTrendTableViewCell.swift
+++ Qiita_Client/View/Cell/HomeTrendTableViewCell.swift
@@ -9,7 +9,20 @@
 import UIKit
 
 class HomeTrendTableViewCell: UITableViewCell {
-
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
