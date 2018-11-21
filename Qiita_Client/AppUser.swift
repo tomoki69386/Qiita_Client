ba0d4142ff20bde05ca84ce63a8e11e48708afc2
tomoki69386 <tomoki_69386@yahoo.co.jp>
2018-11-22 05:56:55 +0900

    Add AppUser

---
+++ Qiita_Client/AppUser.swift
@@ -0,0 +1,13 @@
+//
+//  AppUser.swift
+//  Qiita_Client
+//
+//  Created by 築山朋紀 on 2018/11/22.
+//  Copyright © 2018 tomoki. All rights reserved.
+//
+
+import SwiftyUserDefaults
+
+struct AppUser {
+    private init() {}
+}
