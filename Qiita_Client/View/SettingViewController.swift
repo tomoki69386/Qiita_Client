df5c19a4aec9dcc837c249269c29b051dfe527bd
tomoki69386 <tomoki_69386@yahoo.co.jp>
2018-11-22 06:15:21 +0900

    Create 設定画面

---
+++ Qiita_Client/View/SettingViewController.swift
@@ -0,0 +1,41 @@
+//
+//  SettingViewController.swift
+//  Qiita_Client
+//
+//  Created by 築山朋紀 on 2018/11/22.
+//  Copyright © 2018 tomoki. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+    
+    @IBOutlet private weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.title = "設定"
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView()
+    }
+}
+
+extension SettingViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SettingCell", for: indexPath)
+        cell.textLabel?.text = String(indexPath.row)
+        return cell
+    }
+}
+
+extension SettingViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
+}
