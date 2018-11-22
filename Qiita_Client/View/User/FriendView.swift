78ab6f140b79cab584f15c4d59e006468287ccee
tomoki69386 <tomoki_69386@yahoo.co.jp>
2018-11-22 09:37:47 +0900

    Add Button

--- Qiita_Client/View/User/FriendView.swift
+++ Qiita_Client/View/User/FriendView.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import RxSwift
 
 class FriendView: UIView {
+    
+    let disposeBag = DisposeBag()
     
     private let followCountLabel: UILabel = {
         let label = UILabel()
@@ -46,6 +49,9 @@ class FriendView: UIView {
         return label
     }()
     
+    private let followButton = UIButton()
+    private let followerButton = UIButton()
+    
     private let rodView = UIView()
 
     required override init(frame: CGRect) {
@@ -58,6 +64,18 @@ class FriendView: UIView {
         self.addSubview(followerCountLabel)
         self.addSubview(followerLabel)
         self.addSubview(rodView)
+        self.addSubview(followButton)
+        self.addSubview(followerButton)
+    }
+    
+    private func action() {
+        followButton.rx.tap.subscribe(onNext: { _ in
+            
+        }).disposed(by: disposeBag)
+        
+        followerButton.rx.tap.subscribe(onNext: { _ in
+            
+        }).disposed(by: disposeBag)
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -72,6 +90,8 @@ class FriendView: UIView {
         rodView.frame = CGRect(x: followCountLabel.frame.maxX - 0.5, y: 0, width: 1, height: self.frame.height)
         followerCountLabel.frame = CGRect(x: followCountLabel.frame.maxX, y: 0, width: followCountLabel.frame.width, height: 20)
         followerLabel.frame = CGRect(x: followCountLabel.frame.maxX, y: followerCountLabel.frame.maxY, width: followCountLabel.frame.width, height: 15)
+        followButton.frame = CGRect(x: 0, y: 0, width: followCountLabel.frame.width, height: 35)
+        followerButton.frame = CGRect(x: followerCountLabel.frame.minX, y: 0, width: followerCountLabel.frame.width, height: 35)
     }
 
 }
