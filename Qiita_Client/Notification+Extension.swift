b0db13761bd827f0c093ef7db06538489ba868a5
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-02-05 12:22:03 +0900

    Delete 使ってないファイル

--- Qiita_Client/Notification+Extension.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Notification+Extension.swift
-//  Qiita_Client
-//
-//  Created by 築山朋紀 on 2018/12/31.
-//  Copyright © 2018 tomoki. All rights reserved.
-//
-
-import Foundation
--- Qiita_Client/View/HomeCell.swift
+++
@@ -1,9 +0,0 @@
-//
-//  HomeCell.swift
-//  Qiita_Client
-//
-//  Created by 築山朋紀 on 2018/11/04.
-//  Copyright © 2018 tomoki. All rights reserved.
-//
-
-import Foundation
